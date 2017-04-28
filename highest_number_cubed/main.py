ed43cb41386c67c06f3a68b701b9d576412169dd
Danny C <brasqo@gmail.com>
2017-04-28 01:44:40 +0000

    We did it Reddit!

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,36 @@
 """This is the entry point of the program."""
 
 
+# My attempt...Failed miserably.
+# def highest_number_cubed(limit):
+    
+# #testresult = 12000
+    
+#     test2 = int(limit**(1./3.))
+    
+#     if test2**3 == limit:
+#         print ('ok ' + str(test2) + '.')
+#     else:
+#         if test2**3 == limit:
+#           return 'nope'
+
+# # print(highest_number_cubed(22))
+
+#Jason while solution
+
 def highest_number_cubed(limit):
-    pass
+    
+    number = 0
+    
+    while True:
+        number += 1
+        if number ** 3 > limit:
+            return number - 1
+
+print(highest_number_cubed(12000))
+
+
+# 1 ^ 3 = 1 <30? True...keep going 
+# 2 ^ 3 = 8 <30? True...Keep going
+# 3 ^ 3 = 27 <30? True...keep going
+# 4 ^ 3 = 64 <30? False...Flag, refer to previous value
